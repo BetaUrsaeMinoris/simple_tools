e0538352ff19c0f2f59fc9fbc330ada061d6fa08
lijinquan123 <3291742120@qq.com>
2024-01-29 20:36:45 +0800

    优化插件获取函数get_plugin_map

--- simple_tools/commons.py
+++ simple_tools/commons.py
@@ -189,12 +189,12 @@ class FunctionResult(object):
 
 
 def get_plugin_map(base_cls: type, filter_stems: tuple = None, module: str = None, **kwargs):
+    logger = kwargs.get('logger') or default_logger
     cls_file = Path(inspect.getfile(base_cls))
     if module is None:
         module = base_cls.__module__
         if cls_file.stem != '__init__' and module[-len(cls_file.stem):] == cls_file.stem:
             module = module[:-len(cls_file.stem) - 1]
-    logger = kwargs.get('logger') or default_logger
     if filter_stems is None:
         filter_stems = '__init__', cls_file.stem
     for file in cls_file.parent.iterdir():
@@ -205,18 +205,24 @@ def get_plugin_map(base_cls: type, filter_stems: tuple = None, module: str = Non
                 logger.exception(e)
     plugin_map = {}
 
+    def add_plugin(cls):
+        usable = getattr(cls, 'usable', None)
+        if not usable:
+            return False
+        provider = getattr(cls, 'provider', None)
+        if not isinstance(provider, str):
+            provider = cls.__module__.rsplit('.', 1)[-1]
+        plugin_map[provider] = cls
+        return True
+
     def fill_support_plugin(cls):
+        if not add_plugin(cls):
+            return
         for subclass in cls.__subclasses__():
+            if not add_plugin(subclass):
+                continue
             if subclass.__subclasses__():
                 fill_support_plugin(subclass)
-            else:
-                usable = getattr(subclass, 'usable', None)
-                if not usable:
-                    continue
-                provider = getattr(subclass, 'provider', None)
-                if not isinstance(provider, str):
-                    provider = subclass.__module__.rsplit('.', 1)[-1]
-                plugin_map[provider] = subclass
 
     fill_support_plugin(base_cls)
     return dict(sorted(plugin_map.items(), key=lambda x: x[0]))
